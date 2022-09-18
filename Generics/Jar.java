8a1c07de3eacf51f1dff5609c404ef5ce85fe4ba
Petya Dimitrova <101112384+PetyaaDimitrova@users.noreply.github.com>
2022-09-18 16:14:17 +0300

    Add files via upload

--- Generics/Jar.java
+++ Generics/Jar.java
@@ -5,6 +5,10 @@ import java.util.ArrayDeque;
 public class Jar<E> {
     public ArrayDeque<E> elements;
 
+    public Jar(ArrayDeque<E> elements) {
+        this.elements = new ArrayDeque<>();
+    }
+
     public void add(E element) {
         elements.push(element);
     }
